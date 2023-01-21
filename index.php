2f2910564929ebb9622716612337d51172f576cb
oaleekis <oalexsanderm@gmail.com>
2023-01-21 00:20:26 -0300

    alteracao de tema e remocao de texto/logo

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 <body>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Sistema</a>
+            <a class="navbar-brand" href="index.php"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
